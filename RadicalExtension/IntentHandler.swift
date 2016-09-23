5c8254ff155f396520fac252bfbe2504432603c1
Vinit Jasoliya <vinit5320@gmail.com>
2016-09-23 19:51:36 -0400

    Radical
    SiriKit

---
+++ RadicalExtension/IntentHandler.swift
@@ -0,0 +1,64 @@
+//
+//  IntentHandler.swift
+//  RadicalExtension
+//
+//  Created by Vinit Jasoliya on 9/23/16.
+//  Copyright © 2016 ViNiT. All rights reserved.
+//
+
+import Intents
+
+// As an example, this class is set up to handle Message intents.
+// You will want to replace this or add other intents as appropriate.
+// The intents you wish to handle must be declared in the extension's Info.plist.
+
+// You can test your example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+// "<myApp> John saying hello"
+// "Search for messages in <myApp>"
+
+class IntentHandler: INExtension, INSendMessageIntentHandling {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+    // MARK: - INSendMessageIntentHandling
+    
+    func resolveRecipients(forSendMessage intent: INSendMessageIntent, with completion: @escaping ([INPersonResolutionResult]) -> Void) {
+        let notRequired = INPersonResolutionResult.notRequired()
+        completion([notRequired])
+    }
+    
+    func resolveContent(forSendMessage intent: INSendMessageIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        if let text = intent.content, !text.isEmpty {
+            completion(INStringResolutionResult.success(with: text))
+        } else {
+            completion(INStringResolutionResult.needsValue())
+        }
+    }
+    
+    // Once resolution is completed, perform validation on the intent and provide confirmation (optional).
+    
+    func confirm(sendMessage intent: INSendMessageIntent, completion: @escaping (INSendMessageIntentResponse) -> Void) {
+        // Verify user is authenticated and your app is ready to send a message.
+        
+        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSendMessageIntent.self))
+        let response = INSendMessageIntentResponse(code: .ready, userActivity: userActivity)
+        completion(response)
+    }
+    
+    // Handle the completed intent (required).
+    
+    func handle(sendMessage intent: INSendMessageIntent, completion: @escaping (INSendMessageIntentResponse) -> Void) {
+        // Implement your application logic to send a message here.
+        
+        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSendMessageIntent.self))
+        let response = INSendMessageIntentResponse(code: .success, userActivity: userActivity)
+        completion(response)
+    }
+}
+
---
+++ RadicalExtensionUI/IntentViewController.swift
@@ -0,0 +1,56 @@
+//
+//  IntentViewController.swift
+//  RadicalExtensionUI
+//
+//  Created by Vinit Jasoliya on 9/23/16.
+//  Copyright © 2016 ViNiT. All rights reserved.
+//
+
+import IntentsUI
+
+// As an example, this extension's Info.plist has been configured to handle interactions for INSendMessageIntent.
+// You will want to replace this or add other intents as appropriate.
+// The intents whose interactions you wish to handle must be declared in the extension's Info.plist.
+
+// You can test this example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+
+class IntentViewController: UIViewController, INUIHostedViewControlling, INUIHostedViewSiriProviding {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - INUIHostedViewControlling
+    
+    // Prepare your view controller for the interaction to handle.
+    func configure(with interaction: INInteraction!, context: INUIHostedViewContext, completion: ((CGSize) -> Void)!) {
+        // Do configuration here, including preparing views and calculating a desired size for presentation.
+        
+        let intent = interaction.intent as! INSendMessageIntent
+        messageLabel.text = intent.content
+        
+        if let completion = completion {
+            completion(self.desiredSize)
+        }
+    }
+    
+    var desiredSize: CGSize {
+        return self.extensionContext!.hostedViewMaximumAllowedSize
+    }
+    
+    //Displays my custom message UI
+    
+    var displaysMessage: Bool {
+        return true
+    }
+    
+}
